39a6b95f4ca52f9842fe390d05f8efd3f16574a5
JT <jt@usekojo.com>
2023-12-02 07:48:32 -0800

    saving work

--- 01/main.hs
+++ 01/main.hs
@@ -1,21 +1,53 @@
 import System.IO
 import Data.Char (ord)
 import Data.Text (replace)
+import Data.List (isPrefixOf, find)
+import Data.Maybe (catMaybes)
 
-ord0 = ord '0'
-ord9 = ord '9'
+numberChars = ['0'..'9']
 
 isCharNumber :: Char -> Bool
-isCharNumber x = ordX >= ord0 && ordX <= ord9
-  where
-    ordX = ord x
+isCharNumber ch = elem ch numberChars
 
 extractNumbers :: String -> String
 extractNumbers = filter isCharNumber
 
 -- part 2 helpers
+numberStrings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+numberStringToDigit :: String -> Char
+numberStringToDigit str
+  | str == "zero" = '0'
+  | str == "one" = '1'
+  | str == "two" = '2'
+  | str == "three" = '3'
+  | str == "four" = '4'
+  | str == "five" = '5'
+  | str == "six" = '6'
+  | str == "seven" = '7'
+  | str == "eight" = '8'
+  | str == "nine" = '9'
+
+
+matchingPrefix :: [String] -> String -> Maybe String
+matchingPrefix words str = find (`isPrefixOf` str) words
 
 -- func to traverse string until finding a token
+findMatch :: String -> Maybe Char
+findMatch [] = Nothing
+findMatch [x] = if isCharNumber x then Just x else Nothing
+findMatch list@(x:xs) = 
+    case matchingWord of
+      Just str -> Just (numberStringToDigit str)
+      Nothing -> case findMatch [x] of
+        Just y -> Just y
+        Nothing -> findMatch xs
+  where
+    matchingWord = matchingPrefix numberStrings list
+
+sumMaybeTuples :: (Maybe Char, Maybe Char) -> Maybe Int
+sumMaybeTuples (Just ch1, Just ch2) = Just (read ([ch1] ++ [ch2]))
+sumMaybeTuples _ = Nothing
 
 -- From root of repository:
 -- runhaskell 01/main.hs
@@ -23,13 +55,24 @@ main = do
   handle <- openFile "01/input.txt" ReadMode
   contents <- hGetContents handle
 
+  let lists = lines contents
+
   -- part 1
-  let numlines = map extractNumbers (lines contents)
+  let numlines = map extractNumbers lists
   let digitsToSum = map (\line -> read([head line] ++ [last line])) numlines
   let total = sum digitsToSum
   print "Part 1"
   print total
 
   -- part 2
+  let numPairs = map (\line -> (findMatch line, findMatch (reverse line))) lists
+  -- print numLinesPart2
+  let maybeSums = map sumMaybeTuples numPairs
+  print maybeSums
+  let sums = catMaybes maybeSums
+  print sums
+  let totalPart2 = sum sums
+  print "Part 2"
+  print totalPart2
 
   hClose handle
---
+++ 02/main.hs
@@ -0,0 +1,73 @@
+import System.IO
+import Data.Char (isDigit)
+import Data.List.Split (splitOn)
+import Data.List (isSuffixOf, find)
+
+data Turn = Turn { getRed :: Int, getGreen :: Int, getBlue :: Int } deriving(Show)
+
+data Game = Game { getIndex :: Int, getTurns :: [Turn] } deriving(Show)
+
+parseGame :: String -> Game
+parseGame str = Game { getIndex = index, getTurns = turns }
+  where
+    (gameDef, rest) = splitOnGameIndexDefinition str
+    index = extractNumberFromStart (drop 5 gameDef)
+    turnStrings = splitOn "; " rest
+    turns = map parseTurn turnStrings
+
+parseTurn :: String -> Turn
+parseTurn string = Turn { getRed = red, getGreen = green, getBlue = blue }
+  where
+    colorStrings = splitOn ", " string
+    redString = find (\s -> "red" `isSuffixOf` s) colorStrings
+    red = case redString of
+      Just str -> extractNumberFromStart str
+      Nothing -> 0
+    greenString = find (\s -> "green" `isSuffixOf` s) colorStrings
+    green = case greenString of
+      Just str -> extractNumberFromStart str
+      Nothing -> 0
+    blueString = find (\s -> "blue" `isSuffixOf` s) colorStrings
+    blue = case blueString of
+      Just str -> extractNumberFromStart str
+      Nothing -> 0
+
+splitOnGameIndexDefinition :: String -> (String, String)
+splitOnGameIndexDefinition str =
+    let (before, after) = break (== ':') str
+    in if null after
+       then (before, "")
+       else (before, drop 2 after) -- drop 2 to remove the ": " from the start of 'after'
+
+extractNumberFromStart :: String -> Int
+extractNumberFromStart str = read numberStr
+  where 
+    numberStr = takeWhile (isDigit) $ str
+
+isValidTurn :: (Int, Int, Int) -> Turn -> Bool
+isValidTurn (red, green, blue) turn = isRedValid && isGreenValid && isBlueValid
+  where
+    isRedValid = red >= getRed turn
+    isBlueValid = blue >= getBlue turn
+    isGreenValid = green >= getGreen turn
+    
+isValidGame :: (Int, Int, Int) -> Game -> Bool
+isValidGame validityTuple game = all (isValidTurn validityTuple) (getTurns game)
+
+sumValidGameIndices :: (Int, Int, Int) -> [Game] -> Int
+sumValidGameIndices validityTuple games = sum indices
+  where
+    validGames = filter (isValidGame validityTuple) games
+    indices = map getIndex validGames
+    
+main = do
+  handle <- openFile "02/input.txt" ReadMode
+  contents <- hGetContents handle
+
+  -- part 1
+  let rawGames = lines contents
+  let games = map parseGame rawGames
+  print "Part 1"
+  print $ sumValidGameIndices (12, 13, 14) games
+
+  hClose handle
